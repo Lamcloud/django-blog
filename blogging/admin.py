18c0834825dd7676db79d54dc3d54df9fbea5dde
Sara Lam <saylam@iu.edu>
2022-09-05 23:38:52 -0500

    Commit changes to admin.py

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,13 +2,34 @@ from django.contrib import admin
 
 from blogging.models import Post, Category
 
-# and a new admin registration
-admin.site.register(Post)
+class MembershipInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        MembershipInline,
+    ]
 
 class CategoryAdmin(admin.ModelAdmin):
-    fields = ['name', 'description']
 
-class CategoryAdmin(admin.ModelAdmin):
-    exclude = ['posts']
+    exclude = ('posts',)
 
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+
+# from django.contrib import admin
+#
+# from blogging.models import Post, Category
+#
+# # and a new admin registration
+# admin.site.register(Post)
+#
+# class CategoryAdmin(admin.ModelAdmin):
+#     fields = ['name', 'description']
+#
+# class CategoryAdmin(admin.ModelAdmin):
+#     exclude = ['posts']
+#
+# admin.site.register(Category, CategoryAdmin)
+
