cfccbd813eb12a96ce51cc2dcd3af7ba745d9c54
Sara Lam <saylam@iu.edu>
2022-09-05 22:43:11 -0500

    Commit changes to admin.py

--- blogging/admin.py
+++ blogging/admin.py
@@ -4,14 +4,6 @@ from blogging.models import Post, Category
 
 # and a new admin registration
 admin.site.register(Post)
-#admin.site.register(Category)
-
-# @admin.register(Post)
-# class PostAdmin(admin.ModelAdmin):
-#     pass
-    #fields = ['category', 'title', 'text', 'author']
-
-#admin.site.register(Post, PostAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
     fields = ['name', 'description']
@@ -20,13 +12,3 @@ class CategoryAdmin(admin.ModelAdmin):
     exclude = ['posts']
 
 admin.site.register(Category, CategoryAdmin)
-
-# class CategoryInline(admin.TabularInline):
-#     model = Category
-#
-# class PostAdmin(admin.ModelAdmin):
-#     inlines = [
-#         CategoryInline,
-#     ]
-#
-# admin.site.register(Post, PostAdmin)
