4c9ea41e69ddaebff92c50e2e1e603243386ee87
Sara Lam <saylam@iu.edu>
2022-09-18 13:32:38 -0500

    Commit changes

--- blogging/urls.py
+++ blogging/urls.py
@@ -6,7 +6,7 @@ from blogging.views import BlogListView, BlogDetailView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name="blog_index"),
-    path('posts/<int:pk>', BlogDetailView.as_view(), name="blog_detail")
+    path('posts/<int:pk>/', BlogDetailView.as_view(), name="blog_detail")
 ]
 
 # urlpatterns = [path('', list_view, name="blog_index"),
